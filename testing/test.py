28afc1d79bf4402f59a28c95e8e6faa11bfc0745
geugon <geugon@gmail.com>
2014-06-08 17:56:19 -0700

    created editTests

--- testing/test.py
+++ testing/test.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import copy
 import tableTools
 
 
@@ -9,7 +10,7 @@ import tableTools
 class IOTest(unittest.TestCase):
 
 
-	def setUp(self):
+	def setup(self):
 		parser = tableTools.PlainTextParser('\t')
 		self.table = tableTools.Table(parser)
 		self.validFname='input_valid.txt'
@@ -26,6 +27,18 @@ class IOTest(unittest.TestCase):
 			os.remove(self.outputFname)
 		except OSError:
 			pass
+
+
+	def setUp(self):
+		parser = tableTools.PlainTextParser('\t')
+		self.table = tableTools.Table(parser)
+		self.validFname='input_valid.txt'
+		self.emptyFname='input_empty.txt'
+		self.onelineFname='input_oneline.txt'
+		self.inconsistantFname='input_inconsistantColumns.txt'
+		self.repeatedFname='input_repeatedLabels.txt'
+		self.outputFname='output.txt'
+
 
 
 	def test_load_valid(self):
@@ -78,14 +91,58 @@ class EditTest(unittest.TestCase):
 	
 	def setUp(self):
 		parser = tableTools.PlainTextParser('\t')
-		table = tableTools.Table(parser)
-		table.load('input_valid.txt')
+		self.table = tableTools.Table(parser)
+		self.table.load('input_valid.txt')
 
 
-	def test_get(self): pass
-	def test_set(self): pass
-	def test_apply_func(self): pass
-	def test_column_contains(self): pass
+	def tearDown(self):
+		self.table = None
+
+
+	def test_get(self):
+		from_call = self.table.get("mixed_1")
+		from_data = self.table._data[-2]
+		self.assertEqual(from_call,from_data)
+
+
+	def test_set_new(self):
+		self.table.set("new","value")
+		self.assertEqual(4*["value"],self.table._data[-1])
+		self.assertEqual("new",self.table._labels[-1])
+		self.assertEqual("new",self.table._publicLabels[-1])
+
+
+	def test_set_overwrite(self):
+		self.table.set("name","value")
+		self.assertEqual(4*["value"],self.table._data[0])
+		
+
+	def test_apply_func_copy(self):
+		func = lambda x: x
+		self.table.apply_func(func,['mixed_2'],'mixed_3')
+		self.assertEqual(self.table._data[-1],self.table._data[-2])
+
+
+	def test_apply_func_inplace(self):
+		func = lambda x: 2*x
+		expected = [ 2*x for x in self.table._data[1] ]
+		self.table.apply_func(func,['int_1'],'int_1')
+		self.assertEqual(expected,self.table._data[1])
+
+		
+	def test_apply_func_multiargs(self):
+		def func (a,b): return a+b
+		self.table.apply_func(func,['int_1','int_2'],'int_3')
+		expected = [ func(*args) for args in zip(self.table._data[1],self.table._data[2]) ]
+		self.assertEqual(expected,self.table._data[-1])
+		
+
+	def test_column_contains_true(self):
+		assertEqual(self.table.column_contain('int_1',1),True)
+
+
+	def test_column_contains_false(self):
+		assertEqual(self.table.column_contain('int_1',5),False)
 
 	
 ########################################
@@ -96,8 +153,8 @@ class LabelTest(unittest.TestCase):
 	
 	def setUp(self):
 		parser = tableTools.PlainTextParser('\t')
-		table = tableTools.Table(parser)
-		table.load('input_valid.txt')
+		self.table = tableTools.Table(parser)
+		self.table.load('input_valid.txt')
 
 
 	def test_get_public_labels(self): pass
@@ -114,64 +171,8 @@ class AddDropTest(unittest.TestCase):
 
 	def setUp(self):
 		parser = tableTools.PlainTextParser('\t')
-		table = tableTools.Table(parser)
-		table.load('input_valid.txt')
-
-
-	def test_get_public_labels(self): pass
-	def test_match(self): pass
-	def test_remove_row(self): pass
-	def test_generate_row(self): pass
-	def test_merge_rows(self): pass
-	def test_merge_columns(self): pass
-
-
-########################################
-
-
-class EditTest(unittest.TestCase): 
-
-	
-	def setUp(self):
-		parser = tableTools.PlainTextParser('\t')
-		table = tableTools.Table(parser)
-		table.load('input_valid.txt')
-
-
-	def test_get(self): pass
-	def test_set(self): pass
-	def test_apply_func(self): pass
-	def test_column_contains(self): pass
-
-	
-########################################
-
-
-class LabelTest(unittest.TestCase): 
-
-	
-	def setUp(self):
-		parser = tableTools.PlainTextParser('\t')
-		table = tableTools.Table(parser)
-		table.load('input_valid.txt')
-
-
-	def test_get_public_labels(self): pass
-	def test_set_public_labels(self): pass
-	def test_hide(self): pass
-	def test_show(self): pass
-
-
-########################################
-
-
-class AddDropTest(unittest.TestCase):
-
-
-	def setUp(self):
-		parser = tableTools.PlainTextParser('\t')
-		table = tableTools.Table(parser)
-		table.load('input_valid.txt')
+		self.table = tableTools.Table(parser)
+		self.table.load('input_valid.txt')
 
 
 	def test_get_public_labels(self): pass
